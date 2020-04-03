2ee1e3f5c80c461513bbf00567e36fd11f3fafb0
Billy Wu <billy@korubi.co.nz>
2020-04-03 14:59:15 +1300

    just playing

--- dataprep.R
+++ dataprep.R
@@ -1,8 +1,12 @@
-library(tidyverse)
+library(dplyr)
+library(data.table)
+library(stringr)
+library(tidyr)
 library(lubridate)
+
 library(rvest)
 library(stringdist)
-library(data.table)
+
 
 
 # ---- i. Prepare Data ----
@@ -32,7 +36,7 @@ clean_jhd_to_long <- function(df) {
 }
 
 
-jh_covid19_data <- clean_jhd_to_long(confirmed_raw) %>%
+data <- clean_jhd_to_long(confirmed_raw) %>%
  full_join(clean_jhd_to_long(deaths_raw)) %>%
  full_join(clean_jhd_to_long(recovered_raw)) %>% 
  as.data.table()
@@ -45,19 +49,19 @@ saveRDS(jh_covid19_data, "data/jh_covid19_data.rds")
 
 # un_m49 <- read_html("https://unstats.un.org/unsd/methodology/m49/") %>%
 #  html_table()
-# 
+#
 # un_m49 <- un_m49[[1]]
 # colnames(un_m49) <- c("country_name1", "un_m49", "iso3c")
-# 
+#
 # # Update the lookup table to accomodate for name variations and unrecongised states
 # extra.states <- data.frame(country_name1 = c("Kosovo", "Taiwan"),
 #                            un_m49 = as.integer(c(383, 158)),
 #                            iso3c = c("XKX", "TWN"))
-# 
+#
 # un_m49 <- setDT(rbind(un_m49, extra.states))
-# 
+#
 # un_m49$country_name2 <- copy(un_m49$country_name1)
-# 
+#
 # un_m49[country_name1 == "Bolivia (Plurinational State of)", country_name2 := "Bolivia"]
 # un_m49[country_name1 == "Brunei Darussalam", country_name2 := "Brunei"]
 # un_m49[country_name1 == "Congo", country_name2 := "Congo (Brazzaville)"]
@@ -90,63 +94,63 @@ saveRDS(jh_covid19_data, "data/jh_covid19_data.rds")
 # un_m49[country_name1 == "State of Palestine", country_name2 := "West Bank and Gaza"]
 
 # - For testing -
-# jhd_countries <- setDT(tibble(country = unique(jh_covid19_data$country)) %>% arrange(country)) 
+# jhd_countries <- setDT(tibble(country = unique(jh_covid19_data$country)) %>% arrange(country))
 # jhd_countries[, iso3c := countryIDs$iso3c[match(country, countryIDs$country_name1)]]
 # jhd_countries[is.na(iso3c), iso3c := countryIDs$iso3c[match(country, countryIDs$country_name2)]]
-# - - - - - - - - 
+# - - - - - - - -
 
 
 # ---- iii. Merge World Bank / Country Data ----
-# library(wbstats)
-# 
-# pull_worldbank_data <- function(vars) {
-#  new_cache <- wbcache()
-#  all_vars <- as.character(unique(new_cache$indicators$indicatorID))
-#  data_wide <- wb(indicator = vars, mrv = 10, return_wide = TRUE)
-#  new_cache$indicators[new_cache$indicators[,"indicatorID"] %in% vars, ] %>%
-#   rename(var_name = indicatorID) %>%
-#   mutate(var_def = paste(indicator, "\nNote:",
-#                          indicatorDesc, "\nSource:", sourceOrg)) %>%
-#   select(var_name, var_def) -> wb_data_def
-#  new_cache$countries %>%
-#   select(iso3c, iso2c, country, region, income) -> ctries
-#  left_join(data_wide, ctries, by = "iso3c") %>%
-#   rename(year = date,
-#          iso2c = iso2c.y,
-#          country = country.y) %>%
-#   select(iso3c, iso2c, country, region, income, everything()) %>%
-#   select(-iso2c.x, -country.x) %>%
-#   filter(!is.na(NY.GDP.PCAP.KD),
-#          region != "Aggregates") -> wb_data
-#  wb_data$year <- as.numeric(wb_data$year)
-#  wb_data_def<- left_join(data.frame(var_name = names(wb_data),
-#                                     stringsAsFactors = FALSE),
-#                          wb_data_def, by = "var_name")
-#  wb_data_def$var_def[1:6] <- c(
-#   "Three letter ISO country code as used by World Bank",
-#   "Two letter ISO country code as used by World Bank",
-#   "Country name as used by World Bank",
-#   "World Bank regional country classification",
-#   "World Bank income group classification",
-#   "Calendar year of observation"
-#  )
-#  wb_data_def$type = c("cs_id", rep("factor",  4), "ts_id",
-#                       rep("numeric", ncol(wb_data) - 6))
-#  return(list(wb_data, wb_data_def))
-# }
+library(wbstats)
 
-# vars <- c("SP.POP.TOTL", "AG.LND.TOTL.K2", "EN.POP.DNST", "EN.URB.LCTY", "SP.DYN.LE00.IN", "NY.GDP.PCAP.KD")
-# wb_list <- pull_worldbank_data(vars)
-# wb_data <- setDT(wb_list[[1]])
-# wb_data_def <- setDT(wb_list[[2]])
-# 
-# # Use this opportunity to update the country ID lookup table using World Bank labels
-# wb_labels <- unique(wb_data[, .(iso3c, country)])
-# 
-# un_m49[, country_name3 := wb_labels$country[match(iso3c, wb_labels$iso3c)]]
-# un_m49[is.na(country_name3), country_name3 := country_name1]
-# 
-# saveRDS(un_m49, "data/countryIDs.rds")
+pull_worldbank_data <- function(vars) {
+ new_cache <- wbcache()
+ all_vars <- as.character(unique(new_cache$indicators$indicatorID))
+ data_wide <- wb(indicator = vars, mrv = 10, return_wide = TRUE)
+ new_cache$indicators[new_cache$indicators[,"indicatorID"] %in% vars, ] %>%
+  rename(var_name = indicatorID) %>%
+  mutate(var_def = paste(indicator, "\nNote:",
+                         indicatorDesc, "\nSource:", sourceOrg)) %>%
+  select(var_name, var_def) -> wb_data_def
+ new_cache$countries %>%
+  select(iso3c, iso2c, country, region, income) -> ctries
+ left_join(data_wide, ctries, by = "iso3c") %>%
+  rename(year = date,
+         iso2c = iso2c.y,
+         country = country.y) %>%
+  select(iso3c, iso2c, country, region, income, everything()) %>%
+  select(-iso2c.x, -country.x) %>%
+  filter(!is.na(NY.GDP.PCAP.KD),
+         region != "Aggregates") -> wb_data
+ wb_data$year <- as.numeric(wb_data$year)
+ wb_data_def<- left_join(data.frame(var_name = names(wb_data),
+                                    stringsAsFactors = FALSE),
+                         wb_data_def, by = "var_name")
+ wb_data_def$var_def[1:6] <- c(
+  "Three letter ISO country code as used by World Bank",
+  "Two letter ISO country code as used by World Bank",
+  "Country name as used by World Bank",
+  "World Bank regional country classification",
+  "World Bank income group classification",
+  "Calendar year of observation"
+ )
+ wb_data_def$type = c("cs_id", rep("factor",  4), "ts_id",
+                      rep("numeric", ncol(wb_data) - 6))
+ return(list(wb_data, wb_data_def))
+}
+
+vars <- c("SP.POP.TOTL", "AG.LND.TOTL.K2", "EN.POP.DNST", "EN.URB.LCTY", "SP.DYN.LE00.IN", "NY.GDP.PCAP.KD")
+wb_list <- pull_worldbank_data(vars)
+wb_data <- setDT(wb_list[[1]])
+wb_data_def <- setDT(wb_list[[2]])
+
+# Use this opportunity to update the country ID lookup table using World Bank labels
+wb_labels <- unique(wb_data[, .(iso3c, country)])
+
+un_m49[, country_name3 := wb_labels$country[match(iso3c, wb_labels$iso3c)]]
+un_m49[is.na(country_name3), country_name3 := country_name1]
+
+saveRDS(un_m49, "data/countryIDs.rds")
 
 # Implement fussy match if still missing. Remove missing if un-matchable.
 
@@ -157,9 +161,9 @@ wb_data_def <- wb_list[[2]]
 
 varnames <- c("population", "land_area_skm", "pop_density", "pop_largest_city", "life_expectancy", "gdp_capita")
 
-wb_cs <- setDT(wb_data)[, (varnames) := lapply(.SD, function(x) last(na.omit(x))), 
-                       .SDcols = vars, 
-                       by = iso3c][, unique(.SD), 
+wb_cs <- setDT(wb_data)[, (varnames) := lapply(.SD, function(x) last(na.omit(x))),
+                       .SDcols = vars,
+                       by = iso3c][, unique(.SD),
                                    .SDcols = c("iso3c", "country", "region", "income", varnames)]
 
 setnames(wb_cs, "country", "country_name3")
@@ -177,6 +181,10 @@ extra.states <- data.frame(iso3c = "TWN",
                            gdp_capita = as.numeric(24827.898))
 
 wb_cs <- rbind(wb_cs, extra.states)
+
+to.char <- c("iso3c", "country_name3", "region", "income")
+
+wb_cs[, (to.char) := lapply(wb_cs[,to.char,with=F], as.character)]
 
 saveRDS(wb_cs, "data/countryDat.rds")
 
--- visuals.R
+++ visuals.R
@@ -1,17 +1,161 @@
-
+library(dplyr)
+library(data.table)
+library(stringr)
+library(tidyr)
+library(lubridate)
 library(ggplot2)
 library(gghighlight)
-library(dplyr)
 
-# ---- i. Import data & lookups ----
+# ---- i. Import latest data & lookups ----
+
+confirmed_raw <- fread("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+deaths_raw    <- fread("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
+recovered_raw <- fread("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv")
+
+clean_jhd_to_long <- function(df) {
+  df_str <- deparse(substitute(df))
+  var_str <- substr(df_str, 1, str_length(df_str) - 4)
+  
+  df %>% group_by(`Country/Region`) %>%
+    filter(`Country/Region` != "Cruise Ship" & `Country/Region` != "MS Zaandam" & `Country/Region` != "Diamond Princess") %>%
+    select(-`Province/State`, -Lat, -Long) %>%
+    mutate_at(vars(-group_cols()), sum) %>% 
+    distinct() %>%
+    ungroup() %>%
+    rename(Country = `Country/Region`) %>%
+    pivot_longer(
+      -Country, 
+      names_to = "date_str", 
+      values_to = var_str
+    ) %>%
+    mutate(date = mdy(date_str)) %>%
+    select(Country, date, !! sym(var_str)) 
+}
+
+data <- clean_jhd_to_long(confirmed_raw) %>%
+  full_join(clean_jhd_to_long(deaths_raw)) %>%
+  full_join(clean_jhd_to_long(recovered_raw)) %>% 
+  as.data.table()
 
 cid  <- readRDS("data/countryIDs.rds")
 cdat <- readRDS("data/countryDat.rds") 
-data <- readRDS("data/jh_covid19_data.rds")
 
-data[, iso3c := cid$iso3c[match(country, cid$country_name1)]]
-data[is.na(iso3c), iso3c := cid$iso3c[match(country, cid$country_name2)]]
-data[is.na(iso3c), iso3c := cid$iso3c[match(country, cid$country_name3)]]
+data[, iso3c := cid$iso3c[match(Country, cid$country_name1)]]
+data[is.na(iso3c), iso3c := cid$iso3c[match(Country, cid$country_name2)]]
+data[is.na(iso3c), iso3c := cid$iso3c[match(Country, cid$country_name3)]]
+
+data[Country == "Taiwan*", Country := "Taiwan"]
+
+
+# ---- i.  Plot ----
+
+# Settings
+selected.iso3c <- c("NZL", "TWN")
+
+
+# Plot Third degree polynomia interpolation
+data %>%
+  filter(iso3c %in% selected.iso3c) %>% 
+  group_by(Country) %>%
+  # filter(date >= time0 & date - time0 <30) %>% 
+  mutate(time = 1:n()) %>%
+  lapply(., function(x) browser()) %>% 
+  
+  ggplot(., aes(x = time, y = confirmed, color = Country)) +
+  xlab("Days since 100 cases") + ylab("Cumulative cases") +
+  geom_smooth(method = "lm", formula = y ~ poly(x, 3), se = FALSE) +
+  # geom_smooth(method = "loess", se =F) +
+  # geom_line() +
+  theme_minimal() + 
+  labs(title = "Focus on the first month: Confirmed Cases in New Zealand\n")
+
+
+
+
+# Data Preparation
+DF <- data %>% 
+  left_join(CHN_time0, by = "iso3c") %>%
+  mutate(
+    edate_confirmed = as.numeric(date - time0)
+  ) %>%
+  filter(time0 >= 0) %>%
+  group_by(country) %>%
+  filter (n() >= 30) %>%
+  ungroup() %>%
+  left_join(cdat, by = "iso3c") %>% 
+  mutate(
+    confirmed_1e5pop = 1e5*confirmed/population)
+
+# Plot Third degree polynomia interpolation
+DF %>%
+  filter(iso3c %in% selected.iso3c) %>% 
+  group_by(country) %>%
+  filter(date >= time0 & date - time0 <30) %>% 
+  mutate(time = 1:n()) %>%
+  ggplot(., aes(x = time, y = confirmed, color = country)) +
+  xlab("Days since 100 cases") + ylab("Cumulative cases") +
+  geom_smooth(method = "lm", formula = y ~ poly(x, 3), se = FALSE) +
+  theme_minimal() + 
+  labs(title = "Focus on the first month: Confirmed Cases in New Zealand\n")
+
+
+
+data %>%
+  filter(iso3c %in% c("AUS", "NZL", "TWN", "FRA", "ESP")) %>%
+  filter(confirmed >= 100) %>% 
+  group_by(iso3c) %>%
+  mutate(time = 1:n()) %>%
+  ggplot(., aes(x = time, y = confirmed, color = iso3c)) +
+  xlab("Days since 100 cases") + 
+  ylab("Cumulative cases") +
+  geom_smooth(method = "lm", formula = y ~ poly(x, 3), se = FALSE) + 
+  theme_minimal()
+
+
+# Time-zero for each country Where confirmed cases matches or exceeds the first avaiable Chinese case number (548 on 2020-01-22)
+# Also a require each country to have at least 30 days post time zero
+CHN_time0 <- data %>% 
+  group_by(iso3c) %>% 
+  filter(confirmed >= 100) %>% 
+  summarise(time0 = min(date))
+
+DF <- data %>% 
+  left_join(CHN_time0, by = "iso3c") %>%
+  mutate(
+    edate_confirmed = as.numeric(date - time0)
+  ) %>%
+  filter(time0 >= 0) %>%
+  group_by(country) %>%
+  filter (n() >= 30) %>%
+  ungroup() %>%
+  left_join(cdat, by = "iso3c") %>% 
+  mutate(
+    confirmed_1e5pop = 1e5*confirmed/population
+  )
+
+DF %>%
+  filter(region == "East Asia & Pacific" & iso3c != "CHN" & iso3c != "KOR") %>% 
+  group_by(country) %>%
+  filter(date >= time0 & date - time0 <30) %>% 
+  mutate(time = 1:n()) %>%
+  ggplot(., aes(x = time, y = confirmed, color = country)) +
+  xlab("Days since 100 cases") + ylab("Cumulative cases") +
+  geom_line() + theme_minimal() + labs(
+    title = "Focus on the first month: Confirmed Cases\n"
+  )
+
+# Third degree polynomia interpolation
+DF %>%
+  filter(iso3c %in% c("NZL", "TWN")) %>% 
+  group_by(country) %>%
+  filter(date >= time0 & date - time0 <30) %>% 
+  mutate(time = 1:n()) %>%
+  ggplot(., aes(x = time, y = confirmed, color = country)) +
+  xlab("Days since 100 cases") + ylab("Cumulative cases") +
+  geom_smooth(method = "lm", formula = y ~ poly(x, 3), se = FALSE) +
+  theme_minimal() + 
+  labs(title = "Focus on the first month: Confirmed Cases in New Zealand\n")
+
 
 # I define event time zero where, for a given country, the confirmed
 # cases match or exceed the Chinese case number at the beginning of the
@@ -22,6 +166,21 @@ edates_confirmed <- data %>%
  group_by(iso3c) %>%
  filter(confirmed >= min(data$confirmed[data$iso3c == "CHN"])) %>%
  summarise(edate_confirmed = min(date))
+
+df <- data %>% 
+  left_join(edates_confirmed, by = "iso3c") %>%
+  mutate(
+    edate_confirmed = as.numeric(date - edate_confirmed)
+  ) %>%
+  filter(edate_confirmed >= 0) %>%
+  group_by(country) %>%
+  # filter (n() >= 7) %>% 
+  ungroup() %>%
+  left_join(cdat, by = "iso3c") %>% 
+  mutate(
+    confirmed_1e5pop = 1e5*confirmed/population
+  )
+
 
 df <- data %>% 
  left_join(edates_confirmed, by = "iso3c") %>%
